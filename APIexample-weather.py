830e49486ae1cfd334df5ab705900cca1b14d105
Dr Z <zamore@vt.edu>
2020-06-25 16:31:30 -0600

    API demo script added

---
+++ APIexample-weather.py
@@ -0,0 +1,18 @@
+import urllib
+import json
+
+socket = urllib.request.urlopen('https://api.weather.gov/gridpoints/BOU/53,74/forecast ')
+
+
+data = json.loads(socket.read()) #pass the data into a json reader
+#data is now a dictionary
+
+#for weather.gov data, information is stored under the 'properties' key
+#this is a nested dictionary, so data['properties'] also has keys
+
+#we can access nested keys the same way we access nested lists:
+data['properties']['periods'] #returns a list of data by segments (~12 hrs)
+
+data['properties']['periods'][0]['shortForecast'] #returns individual forecast descriptions (str)
+#data['properties']['periods'][0]['name'] #returns the time period (str)
+#data['properties']['periods'][0]['temperature'] #returns the temperature of the time period (int)
